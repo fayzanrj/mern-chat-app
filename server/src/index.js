b2feaf19014724fca0d261b5c994cba08c0ad294
fayzanrj <121216897+fayzanrj@users.noreply.github.com>
2023-04-20 01:02:08 +0500

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const connectToMongo = require('./db');
 const cors = require('cors')
+require("dotenv").config()
 
 const app = express();
 connectToMongo()
-const port = 5000;
+const port = process.env.PORT || 5000
 app.use(cors())
 
 app.get('/', (req, res) => {
